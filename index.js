bf09c0f69d2e5758480c04cc745384f3385288e2
krishnendu basak <krisauz17@yahoo.com>
2022-03-22 18:12:37 +1100

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+let multiply, num1, num2;
+num1 = 2;
+num2 = 31;
+multiply = (num1 * num2);
+//multiply = 62;
+let random = 1;
+Math.round(Math.random() * 1) > 0;
+let mod, num3, num4;
+num3 = 10
+num4 = 6
+mod = (num3 % num4);
+//mod = 4;
+let max= 19;
+if(max=>20, max++) {
+    console.log("return the value"+ max);
+}
